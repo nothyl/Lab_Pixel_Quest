6763ed126e1ff184abe84964381e865b477ddbca
nothyl <hanyuli488@gmail.com>
2025-02-15 11:11:21 -0500

    2/15 c sharp

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -5,17 +5,47 @@ using UnityEditor;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
+
 {
+
+    private Rigidbody2D rb;
+    public int speed = 5;
+
     // Start is called before the first frame update
-    void Start()
+    //private void OnTriggerEnter2D(Collider2D collision);
+    //{Debug.log("Hit");}
+void Start()
     {
-        Debug.Log("Hello World");
+        rb= GetComponent <Rigidbody2D>();
 
     }
+    
 
     // Update is called once per frame
     void Update()
     {
+            
+
+        float xinput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (xinput * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.W))
+        { 
+            transform.position += new Vector3(0, 1, 0); 
+        }
         
-    }
-}
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1, -rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        { 
+            transform.position += new Vector3(0, -1, 0); 
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1, -rb.velocity.y);
+        }
+        }
+        }
