f32c2f22f0944f3e30c89a54dbe78c4ec1d94b40
nothyl <hanyuli488@gmail.com>
2025-02-15 10:13:20 -0500

    ball

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
